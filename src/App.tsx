b2d71012549aa5b94891ae53f779a6371382ec9d
Peter Andresen <pa@onomondo.com>
2022-08-24 11:30:50 +0200

    Add base Code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import spinnerImage from './spinner.png';
+import arrow from './arrow.png';
 import './App.css';
 
+const Spinner1 = () => {
+  return <img src={spinnerImage} className="passive-spin1" alt="logo" />
+}
+
+const Spinner2 = () => {
+  return <img src={spinnerImage} className="passive-spin2" alt="logo" />
+}
+
+const Spinner3 = () => {
+  return <img src={spinnerImage} className="passive-spin3" alt="logo" />
+}
+
+const Spinner4 = () => {
+  return <img src={spinnerImage} className="passive-spin4" alt="logo" />
+}
+
+const Spinner5 = () => {
+  return <img src={spinnerImage} className="passive-spin5" alt="logo" />
+}
+
+const Spinner = ({spinnerNumber}: {spinnerNumber: number}) => {
+  switch (spinnerNumber) {
+    case 1:
+      return <Spinner1 /> //loss
+    case 2:
+      return <Spinner2 />
+    case 3:
+      return <Spinner3 />
+    case 4:
+      return <Spinner4 />
+    case 5:
+      return <Spinner5 />
+    default:
+      return <Spinner1 />
+  }
+}
+
+
+const generateSpinnerNumber = () => {
+  return Math.floor(Math.random() * 5) + 1;
+}
+
 function App() {
+  const [stage, setStage] = useState(0);
+  const [spinnerNumber, setSpinnerNumber] = useState(1);
+
+  const SpinnerWrapper = () => {
+    if (stage === 0) {
+      setSpinnerNumber(generateSpinnerNumber());
+      return <div>
+        <p>Wanna spin?</p>
+        <button onClick={() => setStage(1)} >Yes sir!</button>
+      </div>
+    }
+    if (stage === 1) {
+      return <div>
+        <img src={arrow} className="arrow" alt="arrow" />
+        <Spinner spinnerNumber={spinnerNumber} />
+      </div>
+    }
+    return null
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='spinner'>
+        <SpinnerWrapper />
+      </div>
+      <div className='henrik'>
+
+      </div>
     </div>
   );
 }
