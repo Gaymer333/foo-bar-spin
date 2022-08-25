f3bcfbba15cc46151dee6847aa6da5eede1ea3bf
Peter Andresen <pa@onomondo.com>
2022-08-25 10:22:41 +0200

    MVP

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import spinnerImage from './spinner.png';
 import arrow from './arrow.png';
 import './App.css';
@@ -28,13 +28,13 @@ const Spinner = ({spinnerNumber}: {spinnerNumber: number}) => {
     case 1:
       return <Spinner1 /> //loss
     case 2:
-      return <Spinner2 />
+      return <Spinner2 /> //loss
     case 3:
-      return <Spinner3 />
+      return <Spinner3 /> //loss
     case 4:
-      return <Spinner4 />
+      return <Spinner4 /> //won
     case 5:
-      return <Spinner5 />
+      return <Spinner5 /> //won
     default:
       return <Spinner1 />
   }
@@ -45,22 +45,106 @@ const generateSpinnerNumber = () => {
   return Math.floor(Math.random() * 5) + 1;
 }
 
+const getCoolDown = () => {
+  const coolDown = localStorage.getItem('coolDown');
+  if (coolDown) {
+    return parseInt(coolDown);
+  } else {
+    return new Date().getTime();
+  }
+}
+
+const _setCoolDown = (coolDown: number) => {
+  localStorage.setItem('coolDown', coolDown.toString());
+}
+
+const getStageFromLocalStorage = () => {
+  const stage = localStorage.getItem('stage');
+  if (stage) {
+    return parseInt(stage);
+  } else {
+    return 0;
+  }
+}
+
+const getRandomTime = () => {
+  const min = 10 * 60 * 1000;
+  const max = 20 * 60 * 1000;
+  
+  return Math.floor(Math.random() * (max - min + 1)) + min + new Date().getTime();
+}
+
+// Render secounds in mm:ss format
+const renderTime = (secounds: number) => {
+  const minutes = Math.floor(secounds / 60);
+  const seconds = secounds - minutes * 60;
+  return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+}
+
 function App() {
-  const [stage, setStage] = useState(0);
+  const loadText = '...Loading';
+
+  const [stage, setStage] = useState(getStageFromLocalStorage());
   const [spinnerNumber, setSpinnerNumber] = useState(1);
+  const [coolDown, setCoolDown] = useState(getCoolDown());
+  const [secoundsLeft, setSecondsLeft] = useState<number | string>(loadText);
+  const [spinnedThisSession, setSpinnedThisSession] = useState(false);
+
+  const _setStage = (stage: number) => {
+    localStorage.setItem('stage', stage.toString());
+    setStage(stage);
+  }
+
+  const startCountdown = () => {
+    const countdown = setInterval(() => {
+      const now = new Date().getTime();
+      const localSecondsLeft = Math.floor((coolDown - now) / 1000);
+      setSecondsLeft(localSecondsLeft);
+      if(localSecondsLeft <= 0) clearInterval(countdown)
+    } , 1000);
+  }
+
+  const restartGame = () => {
+    if(secoundsLeft <= 0)
+    {
+      _setStage(0);
+      setSecondsLeft(loadText);
+    }
+  }
 
   const SpinnerWrapper = () => {
     if (stage === 0) {
       setSpinnerNumber(generateSpinnerNumber());
+      setSpinnedThisSession(false);
       return <div>
         <p>Wanna spin?</p>
-        <button onClick={() => setStage(1)} >Yes sir!</button>
+        <button onClick={() => _setStage(1)} >Yes sir!</button>
       </div>
     }
     if (stage === 1) {
+      if(stage === 1) setTimeout(() => _setStage(2), 12000);
       return <div>
         <img src={arrow} className="arrow" alt="arrow" />
         <Spinner spinnerNumber={spinnerNumber} />
+      </div>
+    }
+    if (stage === 2) {
+      if(!spinnedThisSession) {
+        if(coolDown <= new Date().getTime()) {
+          const newCoolDown = getRandomTime();
+          setCoolDown(newCoolDown);
+          _setCoolDown(newCoolDown);
+        }
+        startCountdown();
+        setSpinnedThisSession(true);
+      }
+
+      console.log("secoundsLeft:" + secoundsLeft);
+
+      return <div>
+        <p>{secoundsLeft > 0 || secoundsLeft === loadText ? typeof secoundsLeft === 'string' ? secoundsLeft : renderTime(secoundsLeft) : 'Time to spin again!'}</p>
+        <p>{spinnerNumber < 4 ? 'You lost!' : 'You won!'}</p>
+        <button disabled={secoundsLeft > 0 || secoundsLeft === loadText} onClick={restartGame} >Try again</button>
       </div>
     }
     return null
